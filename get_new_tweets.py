33238c5a073428758bc2974b5ffc1e821fba50c1
laurenshareshian <laurenshareshian@gmail.com>
2017-03-31 15:31:47 -0700

    added tweet to twitter file

--- get_new_tweets.py
+++ get_new_tweets.py
@@ -10,7 +10,7 @@ acct = 'realdonaldtrump'
 twittercount=200 #pull 200 tweets at a time (the max)
 secrets = hidden.oauth()
 full_url=''.join([TWITTER_URL,'?','count=',str(twittercount),'&', 'screen_name=', acct])
-user_timeline = twurl2.oauth_req( full_url, secrets['token_key'], secrets['token_secret'] )
+user_timeline = twurl2.oauth_req( full_url, secrets['token_key'], secrets['token_secret'], "GET" )
 js=json.loads(user_timeline)
 print(0, js[0]['text'], js[0]['id'], js[0]['created_at']) #print most recent tweet
 wrap_list.append(js)
@@ -23,7 +23,7 @@ max_id=js[len(js) - 1]['id']
 while old_max_id!=max_id:
     old_max_id=max_id
     full_url=''.join([TWITTER_URL,'?','count=',str(twittercount),'&', 'screen_name=', acct, '&', 'max_id=', str(max_id)])
-    user_timeline = twurl2.oauth_req( full_url, secrets['token_key'], secrets['token_secret'] )
+    user_timeline = twurl2.oauth_req( full_url, secrets['token_key'], secrets['token_secret'], "GET" )
     js = json.loads(user_timeline)
     print(count, js[0]['text'], js[0]['id'], js[0]['created_at']) #print one every 200 tweets
     max_id=js[len(js) - 1]['id']
