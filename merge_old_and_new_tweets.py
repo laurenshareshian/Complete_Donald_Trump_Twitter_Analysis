9131a82e82777ceb4db9afce8fc805e58490d232
laurenshareshian <laurenshareshian@gmail.com>
2017-03-31 13:25:00 -0700

    updated merge tweets

--- merge_old_and_new_tweets.py
+++ merge_old_and_new_tweets.py
@@ -11,22 +11,24 @@ with open('donalddata_new.json') as json_data2:
     js2 = json.load(json_data2)
 
 #merge both tweets
-js.append(js1)
-js.append(js2)
+for j in range(len(js1)):
+    js.append(js1[j])
+
+for j in range(len(js2)):
+    for i in range(len(js2[j])):
+        js.append(js2[j][i])
 
 #delete duplicates based on creation time
 datelist=[]
 count=0
 dups_removed=[]
+
 for j in range(len(js)):
-    for i in range(len(js[j])):
-        for k in range(len(js[j][i])):
-            date=js[j][i][k]['created_at']
-            if date not in datelist: #remove duplicates
-                datelist.append(date)
-#                print(j, count, js[j][i][k]['text'], js[j][i][k]['id'],js[j][i][k]['created_at'] )
-                count = count + 1
-                dups_removed.append(js[j][i][k])
+    date=js[j]['created_at']
+    if date not in datelist: #remove duplicates
+        datelist.append(date)
+        count = count + 1
+        dups_removed.append(js[j])
 
 
 print('number of unique tweets:', len(dups_removed))
